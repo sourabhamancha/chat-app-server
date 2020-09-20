353aff068b06f23a702de86190c66d7d8ecee9bb
Sourabh Amancha <sourabh.amancha@gmail.com>
2020-09-20 12:36:51 +0530

    adding images

--- schema/Mutation.js
+++ schema/Mutation.js
@@ -95,6 +95,7 @@ module.exports = new GraphQLObjectType({
             username,
             email,
             password: hasedPassword,
+            imageUrl,
           });
 
           // create authentication token
